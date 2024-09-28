ad7602e221a0836afde7b9369dcaf31e88519134
satoshi_honda <hndsts@gmail.com>
2024-09-28 22:10:19 +0900

    ユーザー一覧表示まで記載。一覧がうまく表示できていないのでここで分岐を作る。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resource :sessions, only: [:new, :create, :destroy]
   root to: 'registrations#new'
 
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
